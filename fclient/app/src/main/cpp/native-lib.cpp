7b438807b26f21be42c74b05e2d06916b2cbca3b
spectralisk <spectral663@mail.ru>
2021-02-24 01:39:25 +0300

    commit

--- fclient/app/src/main/cpp/native-lib.cpp
+++ fclient/app/src/main/cpp/native-lib.cpp
@@ -9,10 +9,11 @@
 #include "/Users/spectralisk/AndroidStudioProjects/libs/mbedtls/mbedtls/library/entropy.h"
 #include "/Users/spectralisk/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ctr_drbg.h"
 #include "/Users/spectralisk/AndroidStudioProjects/libs/mbedtls/mbedtls/library/common.h"
-
+/*
 mbedtls_entropy_context entropy;
 mbedtls_ctr_drbg_context ctr_drbg;
 char *personalization = "fclient-sample-app";
+*/
 
 #define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
 auto android_logger = spdlog::android_logger_mt(  "android" , "fclient_ndk");
@@ -22,12 +23,12 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_ru_iu3_fclient_MainActivity_stringFromJNI (
             JNIEnv* env,
             jobject MainActivity /* this */) {
-        std::string hello = "Hello, Nice ... bro!";
+        std::string hello = "Hey, Bro nice!";
         LOG_INFO("Hello from system log %d", 2021);
         SLOG_INFO("Hello from spdlog {}", 2021);
         return env->NewStringUTF(hello.c_str());
 }
-
+/*
 extern "C" JNIEXPORT jint JNICALL
 Java_ru_iu3_fclient_MainActivity_initRng(
         JNIEnv *env,
@@ -53,4 +54,4 @@ Java_ru_iu3_fclient_MainActivity_randomBytes(
     delete[] buf;
     return rnd;
 }
-
+*/
