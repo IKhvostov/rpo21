13d379ee58e3045a8980d631ed775c8f9ca39329
eadm2000 <eadm2000@gmail.com>
2021-03-12 12:42:21 +0300

    comment

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -20,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         byte[] rnd = randomBytes(( 16));
+        byte[] data = {6,7,5,1,2,3,9,8};
+        byte[] encryptetext = encrypt(rnd, data);
+        byte[] decryptetext = decrypt(rnd, encryptetext);
+
 
 // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
     }
+
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -33,4 +40,6 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key,byte[] data);
+    public static native byte[] decrypt(byte[] key,byte[] data);
 }
